e0b2cba1e2071f9ade80e1f62190f5f84a822823
Dev <127723592+DevCoderByjus@users.noreply.github.com>
2023-11-03 18:27:13 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -5,19 +5,20 @@ var recognition = new SpeechRecognition();
 function start()
 {
     recognition.start();
-}
-
+} 
+ 
 recognition.onresult = function(event) {
 
-    console.log(event);
+ console.log(event); 
 
-    Content = event.results[0][0].transcript.toLowerCase();
-    console.log(Content);
-    if (Content == "selfie") {
-      speak();
+ Content = event.results[0][0].transcript.toLowerCase();
+   console.log(Content);
+   if (Content == "selfie") {
+    speak();
     }
 
 }
+
 
 function speak(){
     var synth = window.speechSynthesis;
@@ -27,35 +28,37 @@ function speak(){
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
 
+
     setTimeout(function()
-    {
+    { 
         img_id = "selfie1";
-        take_snapshot();
+        take_snapshot(); 
         speak_data = "Taking your next Selfie in 10 seconds";
         var utterThis = new SpeechSynthesisUtterance(speak_data);
         synth.speak(utterThis);
 
-    }, 5000);
+      }, 5000);
 
     setTimeout(function()
-    {
+    { 
         img_id = "selfie2";
-        take_snapshot();
+        take_snapshot(); 
         speak_data = "Taking your next Selfie in 15 seconds";
         var utterThis = new SpeechSynthesisUtterance(speak_data);
         synth.speak(utterThis);
-
+        
     }, 10000);
 
     setTimeout(function()
-    {
+    { 
         img_id = "selfie3";
-        take_snapshot();
+        take_snapshot(); 
 
     }, 15000);
 
 }
 
+ 
 camera = document.getElementById("camera");
 Webcam.set({
     width:500,
@@ -67,10 +70,10 @@ Webcam.set({
 function take_snapshot()
 {
     console.log(img_id);
-
+    
     Webcam.snap(function(data_uri) {
         if(img_id=="selfie1"){
-            document.getElementById("result1").innerHTML = '<img id="selfie1" src="'+data_uri+'"/>';
+        document.getElementById("result1").innerHTML = '<img id="selfie1" src="'+data_uri+'"/>';
         }
         if(img_id=="selfie2"){
             document.getElementById("result2").innerHTML = '<img id="selfie2" src="'+data_uri+'"/>';
